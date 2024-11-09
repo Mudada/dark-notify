8b9d2f4fdc9cd456dcb95bbf3d0d8feae3307f02
Tangui <mael.nicolas@clever-cloud.com>
2024-11-09 19:32:37 +0100

    yoinked some commits from cormacrelf and added a flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flake for dark-notify";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    dark-notify = {
+      url = "github:cormacrelf/dark-notify?tag=0.1.2";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, dark-notify }:
+    let 
+      system = "aarch64-darwin";
+      version = "0.1.2";
+      pkgs = nixpkgs.legacyPackages.${system};
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    in {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+	pname = manifest.name;
+    	version = manifest.version;
+    	cargoLock.lockFile = ./Cargo.lock;
+    	src = ./.;
+      };
+    };
+}
